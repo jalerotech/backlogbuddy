b26283e2772c123b34add83d917638a69af04ce8
jalero <jalero@cisco.com>
2024-03-27 20:18:03 +0100

    Exemption handling on Zendesk data requests.

--- zendeskProcess/zenDesk.py
+++ zendeskProcess/zenDesk.py
@@ -18,54 +18,65 @@ def get_ticket_per_users(user_email) -> dict[list]:
     :param user_email: jalero@cisco.com
     :return: List of tickets
     """
+
     user_tickets = {}
     logger.info(f"Fetching tickets for the user {user_email} - STARTED")
-    zendesk_response = requests.get(f"{bbc().zendesk_user_url}search.json?query={user_email}",
-                                    headers=bbc().zendesk_headers)
-    zendesk_json_resp = json.loads(zendesk_response.content)
-    agent_id = zendesk_json_resp['users'][0]['id']
+    try:
+        zendesk_response = requests.get(f"{bbc().zendesk_user_url}search.json?query={user_email}",
+                                        headers=bbc().zendesk_headers)
+        zendesk_json_resp = json.loads(zendesk_response.content)
+        agent_id = zendesk_json_resp['users'][0]['id']
 
-    zendesk_url = f'{bbc().zendesk_user_url}{agent_id}/tickets/assigned.json'
-    response = requests.get(zendesk_url, headers=bbc().zendesk_headers)
+        zendesk_url = f'{bbc().zendesk_user_url}{agent_id}/tickets/assigned.json'
+        response = requests.get(zendesk_url, headers=bbc().zendesk_headers)
 
-    # Handle the response
-    if response.status_code == 200:
-        tickets = response.json()['tickets']
-        user_tickets.update({user_email: []})
-        for ticket in tickets:
-            if ticket['status'] != 'closed':
-                updates = get_ticket_updates(ticket)
-                ticket_data = {
-                    'ticket_id': ticket['id'],
-                    'subject': ticket['subject'],
-                    'status': ticket['status'],
-                    'updates': updates
-                }
-                logger.info(f"Ticket ID: {ticket['id']}, Subject: {ticket['subject']}")
-                user_tickets[user_email].append(ticket_data)
+        # Handle the response
+        if response.status_code == 200:
+            tickets = response.json()['tickets']
+            user_tickets.update({user_email: []})
+            for ticket in tickets:
+                if ticket['status'] != 'closed':
+                    updates = get_ticket_updates(ticket)
+                    ticket_data = {
+                        'ticket_id': ticket['id'],
+                        'subject': ticket['subject'],
+                        'status': ticket['status'],
+                        'updates': updates
+                    }
+                    logger.info(f"Ticket ID: {ticket['id']}, Subject: {ticket['subject']}")
+                    user_tickets[user_email].append(ticket_data)
 
-    else:
-        logger.info(f"Failed to retrieve tickets: {response.status_code}")
-    logger.info(f"Fetching tickets for the user {user_email} - COMPLETED")
-    return user_tickets
+        else:
+            logger.info(f"Failed to retrieve tickets: {response.status_code}")
+        logger.info(f"Fetching tickets for the user {user_email} - COMPLETED")
+        return user_tickets
+    except ConnectionError as e:
+        logger.info(f"Connection fails towards Zendesk with error {e.args}.")
+    except TimeoutError as f:
+        logger.info(f"Connection timed out towards Zendesk with error {f.args}.")
 
 
 def get_ticket_updates(ticket):
     # ticket_audit_url = f'{bbc().zendesk_base_url}tickets/{ticket["id"]}/audits.json'
-    ticket_audit_url = f'{bbc().zendesk_base_url}tickets/{ticket["id"]}/comments.json'
-    req_audit = requests.get(ticket_audit_url, headers=bbc().zendesk_headers)
-    ticket_comments = req_audit.json()['comments']
-    comment_data_dict = []
-    for comment in ticket_comments:
-        if comment['type'] == "Comment":
-            if comment['public']:
-                comment_data = {
-                    "body": comment['body'],
-                    "timestamp": comment['created_at'],
-                    "is_public": comment['public']
-                }
-                comment_data_dict.append(comment_data)
-    return comment_data_dict
+    try:
+        ticket_audit_url = f'{bbc().zendesk_base_url}tickets/{ticket["id"]}/comments.json'
+        req_audit = requests.get(ticket_audit_url, headers=bbc().zendesk_headers)
+        ticket_comments = req_audit.json()['comments']
+        comment_data_dict = []
+        for comment in ticket_comments:
+            if comment['type'] == "Comment":
+                if comment['public']:
+                    comment_data = {
+                        "body": comment['body'],
+                        "timestamp": comment['created_at'],
+                        "is_public": comment['public']
+                    }
+                    comment_data_dict.append(comment_data)
+        return comment_data_dict
+    except ConnectionError as e:
+        logger.info(f"Connection fails towards Zendesk (comments URL) with error {e.args}.")
+    except TimeoutError as f:
+        logger.info(f"Connection timed out towards Zendesk (comments URL) with error {f.args}.")
 
 
 if __name__ == '__main__':
